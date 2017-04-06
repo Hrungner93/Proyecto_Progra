55f4e7fdd2c3a6a873421384f3ec885d3e60d6fe
Hrungner93 <irvingmanrob@hotmail.com>
2017-04-05 20:29:51 -0500

    Add project files.

---
+++ Proyecto_Progra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Progra.Startup))]
+namespace Proyecto_Progra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
